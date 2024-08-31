f8923198353832892be67d4635f0cb96a6986444
anuragtech34 <prajapatianurag73240@gmail.com>
2024-08-31 17:10:05 +0530

    First commit

---
+++ Url shortner backend/index.js
@@ -0,0 +1,143 @@
+const express =require('express')
+const cors =require('cors')
+const generateUniqueId = require('generate-unique-id');
+const mongoose=require('mongoose') //mongoose package
+
+const dotenv=require('dotenv')
+dotenv.config()
+const PORT=process.env.PORT || 7000
+
+const app=express();
+app.use(cors())
+app.use(express.json())
+
+//connecting mongodb
+
+mongoose.connect(process.env.MONGODB_STRING)
+
+const urlSchema=new mongoose.Schema({ //schema
+    shortId:{
+        type:String,
+        required:true
+    },
+    originalUrl:{
+        type:String,
+        required:true,
+
+    },
+    shortUrl:{
+        type:String,
+        required:true
+    },
+    visitHistory:[
+        {
+            timestamps:{
+                type:String
+            }
+        }
+    ]
+    
+},{
+    timestamps:true,
+},
+
+)
+
+//model
+
+const ShortUrl=mongoose.model('shortUrls',urlSchema)
+
+app.get('/:shortId',async (req,res)=>{
+
+console.log("request header Host. is",req.headers['host'])
+console.log("request header. From is",req.headers['from'])
+console.log("request header origin. is",req.headers['origin'])
+    const shortId=req.params.shortId
+ await ShortUrl.findOneAndUpdate({shortId:shortId},{$push:{
+    visitHistory:{
+        timestamps:Date.now()
+    }
+ }}).then((shortUrlData)=>{
+
+if(shortUrlData){
+    console.log("short url data is :",shortUrlData)
+
+    return res.redirect(shortUrlData.originalUrl)
+
+}
+else{
+    return res.status(404).send("URL not found")
+}
+    
+
+}).catch(()=>{
+
+    return res.status(500).send("Server error")
+
+})
+
+
+
+
+})
+
+app.post('/analysis',async (req,res)=>{
+
+    const body=req.body
+    const shortUrl=body.url
+console.log('url for ana.',shortUrl)
+const extraClientInfo=req.headers['referer']
+console.log("Client inro:",extraClientInfo)
+
+   await ShortUrl.findOne({shortUrl:shortUrl}).then((shortUrlData)=>{
+
+    if(!shortUrlData){
+        console.log("analysis data not found")
+        return res.status(400)
+     
+    }
+        console.log("short url data for analysis:",shortUrlData)
+        
+        return res.status(200).json({shortUrlData:shortUrlData})
+    }).catch(()=>{
+        return res.status(500).send("Server error")
+    })
+
+
+})
+
+
+app.post('/shorten',async (req,res)=>{
+
+    const body=req.body
+    console.log("Sent req body:",body)
+    const originalUrl=body.url
+console.log("Sent long url:",originalUrl)
+    if(!originalUrl){
+        return res.send("Enter a url")
+    }
+    const shortId=generateUniqueId({
+        length:8,
+    })
+const shortUrl=`http://localhost:${PORT}/${shortId}`
+
+   await ShortUrl.create({
+        shortId:shortId,
+        originalUrl:originalUrl,
+        shortUrl:shortUrl
+    }).then(()=>{
+        return res.status(201).json({shortUrl:shortUrl})
+    }
+    ).catch(()=>{
+        return res.status(500).send("Server error")
+    })
+    
+    
+   
+
+})
+
+
+
+
+app.listen(PORT,()=>console.log("Server started at PORT ",PORT))
---
+++ url-shortner/src/App.jsx
@@ -0,0 +1,146 @@
+import { useContext, useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [currentUrl,setCurrenUrl]=useState();
+  const [shortUrl,setShortUrl]=useState();
+  const [shortURLtoAnalyze,setShortURLtoAnalyze]=useState();
+  const [shortURLData,setShortURLData]=useState();
+
+const createShortenUrl=()=>{
+ 
+  setShortURLtoAnalyze('')
+  setShortURLData('')
+  fetch(`http://localhost:7000/shorten`,{
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json"
+    },
+    body:JSON.stringify({
+      url:currentUrl
+    })
+  }).then(async (response)=>{
+    if(response.ok){
+      const responseData=await response.json()
+      console.log("Successfully URL shorten.")
+      setShortUrl(responseData.shortUrl)
+      console.log("response data is :",responseData)
+      console.log("Generated short url is :",responseData.shortUrl)
+      
+
+    }
+  })
+
+}
+
+const getShortUrlData=()=>{
+
+  fetch('http://localhost:7000/analysis',{
+
+  method:"POST",
+  headers:{
+    "Content-Type":"application/json"
+  },
+
+  body:JSON.stringify({
+    url:shortURLtoAnalyze
+  })
+  }).then(async (response)=>{
+    if(response.ok){
+
+      const responseData=await response.json();
+      console.log("short url data is :",responseData.shortUrlData
+      )
+      setShortURLData(responseData.shortUrlData)
+    }
+
+
+  }).catch(()=>{
+    console.log("Connot get Short url data ")
+  }
+  )
+}
+
+const handleURLInput=(e)=>{
+  const UrlInput=e.target.value;
+  const inputFieldName=e.target.name;
+
+  switch(inputFieldName){
+    case 'shorten-url':
+      setCurrenUrl(UrlInput)
+      break;
+    case 'analyze-short-url':
+      setShortURLtoAnalyze(UrlInput)
+      break;
+  }
+ 
+}
+
+const handleShortnBtnClick=()=>{
+  if(currentUrl){
+    createShortenUrl()
+
+  }
+  else{
+    alert("Please Enter URL")
+  }
+
+}
+
+const handleGetAnalysisBtnClick=()=>{
+  if(shortURLtoAnalyze){
+    getShortUrlData()
+
+  }
+  else{
+    alert("Please Enter your short URL")
+  }
+}
+
+  return (
+    <div className='main-container'>
+
+      <h1 className='main-heading' >Convert your long URls into easy Short URLs</h1>
+
+      <h3 className='headings'>Long URLs to Short URLs </h3>
+    <div className='container'>
+      
+      <input onChange={handleURLInput} name='shorten-url' value={currentUrl} className="url-input" placeholder='Enter your URL'></input>
+
+      <button onClick={handleShortnBtnClick} className='button'>Short</button>
+
+    </div>
+    <p>{shortUrl}</p>
+    <hr></hr>
+    <h3 className='headings'> Get Short URL Analytics</h3>
+    <div className='container'>
+      
+      <input onChange={handleURLInput} name='analyze-short-url'  className="url-input" value={shortURLtoAnalyze} placeholder='Enter your Short URL'></input>
+
+      <button onClick={handleGetAnalysisBtnClick} className='button'>Get Analysis</button>
+
+    </div>
+
+    {
+   
+      (shortURLData)?<div>
+<p>Total visit:{shortURLData.visitHistory
+        .length}</p>
+        <p>Created At: {shortURLData.createdAt}</p>
+        <p>Last Updated At: {shortURLData.updatedAt}</p>
+        <p>Last visit At: {
+        
+        shortURLData.visitHistory.length>0?   Date(shortURLData.visitHistory[shortURLData.visitHistory.length-1].timestamps):'No visit'
+        
+        }</p>
+
+      </div>:''
+    }
+    </div>
+  )
+}
+
+export default App
